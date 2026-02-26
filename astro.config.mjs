6d6af8367f3d6c68be62f79034a471a95b3eb57e
cmolina-dev <ingeniero.carlosmolina184@gmail.com>
2026-02-25 23:45:26 -0500

    fix astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://compendium.local', // Cambiar por tu dominio real
+  site: "https://estudio.carlosmolina.dev", // Cambiar por tu dominio real
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
-      wrap: true
-    }
-  }
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
 });
